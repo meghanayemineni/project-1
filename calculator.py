f50df2df33ae096f705670800735b79f774e39ee
Meghana <meghanayeminani@gmail.com>
2026-09-10 14:47:09 +0530

    Update

--- calculator.py
+++ calculator.py
@@ -1,26 +1,32 @@
 num1=int(input())
 num2=int(input())
+
 op=input(" Enter operator +, -, *, /, //, **, % :") 
+
 if op=="+":
     result=num1+num2
-    print(result) 
+    print(f"result")
+
 elif op=="-":
     result=num1-num2
-    print(result)
+    print(f"result")
+
 elif op=="*":
     result=num1*num2 
-    print(result)  
+    print(f"result")
+
 elif op=="/":
     result=num1/num2
-    print(result)
+    print(f"result")
+
 elif op=="%":
     result=num1%num2 
-    print(result)
+    print(f"result")
+
 elif op=="//":
     result=num1//num2
-    print(result)
+    print(f"result")
+
 elif op=="**":
     result=num1**num2 
-    print(result) 
-else:
-    print("invalid number") 
+    print(f"result") 
